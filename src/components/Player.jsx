370f520a8ecb2edf426814e2cfc89c17f74685ba
Nils Matic <matic1909@gmail.com>
2020-11-06 19:48:05 +0100

    added initial styling to song and player components

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,63 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
+const PlayerStyle = styled.div`
+  min-height: 20vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  .time-control {
+    width: 50%;
+    display: flex;
+
+    input {
+      width: 100%;
+    }
+
+    p {
+      padding: 1rem;
+    }
+  }
+
+  .play-control {
+    width: 30%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+  }
+
+  svg {
+    cursor: pointer;
+  }
+`;
 
 const Player = () => {
   return (
-    <>
+    <PlayerStyle>
       <div className="time-control">
         <p>Start Time</p>
         <input type="range" />
         <p>End Time</p>
       </div>
-      <div className="play-control"></div>
-    </>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
+      </div>
+    </PlayerStyle>
   );
 };
 
--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const SongStyle = styled.div`
+  min-height: 60vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const Song = () => {
   return (
-    <>
+    <SongStyle>
       <h1>Picture</h1>
       <h2>Song Name</h2>
       <h2>Artist</h2>
-    </>
+    </SongStyle>
   );
 };
 
