0c653d2ea3d89d153ab6f5354569c387e6161d11
Nils Matic <matic1909@gmail.com>
2020-11-06 20:13:04 +0100

    added song data and state, passed current song to Song component and added styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import GlobalStyle from "./styles/GlobalStyle";
+import data from "./data";
 
 function App() {
+  const songs = data;
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
       <GlobalStyle />
-      <Song />
+      <Song song={currentSong} />
       <Player />
     </div>
   );
--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -5,15 +5,28 @@ const SongStyle = styled.div`
   min-height: 60vh;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
+
+  img {
+    width: 20%;
+    border-radius: 50%;
+  }
+
+  h2 {
+    padding: 3rem 1rem 1rem 1rem;
+  }
+  h3 {
+    font-size: 1rem;
+  }
 `;
 
-const Song = () => {
+const Song = ({ song }) => {
   return (
     <SongStyle>
-      <h1>Picture</h1>
-      <h2>Song Name</h2>
-      <h2>Artist</h2>
+      <img src={song.cover} alt={song.name} />
+      <h2>{song.name}</h2>
+      <h3>{song.artist}</h3>
     </SongStyle>
   );
 };
